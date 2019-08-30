b098a65666f0c523a17b08d627e507c1edb5133a
Isaiah Santala <bigboyswagcakes@Isaiahs-MacBook-Pro.local>
2019-08-30 11:28:18 -0700

    added ship image, basic physics, and disconnect functionality

--- client/js/game.js
+++ client/js/game.js
@@ -20,14 +20,50 @@ const config = {
 const game = new Phaser.Game(config)
 
 function preload() {
-
+  this.load.image('ship', 'assets/spaceShips_001.png')
 }
 
 function create() {
+  const self = this
   this.socket = io()
+  this.otherPlayers = this.physics.add.group()
+
+  this.socket.on('currentPlayers', function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id])
+      } else {
+        addOtherPlayers(self, players[id])
+      }
+    })
+  })
+
+  this.socket.on('newPlayer', function (playerInfo) {
+    addOtherPlayers(self, playerInfo)
+  })
+
+  this.socket.on('disconnect', function (playerId) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy()
+      }
+    })
+  })
 }
 
 function update() {
 
+}
+
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40)
+  if (playerInfo.team === 'blue') {
+    self.ship.setTint(0x0000ff)
+  } else {
+    self.ship.setTint(0xff0000)
+  }
+  self.ship.setDrag(100)
+  self.ship.setAngularDrag(100)
+  self.ship.setMaxVelocity(200)
 }
 
